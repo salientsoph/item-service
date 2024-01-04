c84b69208713d38bf7c8718cad1d185e31585476
salientsoph <salientsoph@gmail.com>
2024-01-04 16:38:27 +0900

    add html

---
+++ src/main/java/hello/itemService/domain/item/ItemRepository.java
@@ -0,0 +1,40 @@
+package hello.itemService.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>(); //static  //지금은 단일스레드이지만, 다수스레드인경우 ConcorrentHashMap
+    private static long sequence = 0L; //static
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id){
+        return store.get(id);
+    }
+
+    public List<Item>findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
